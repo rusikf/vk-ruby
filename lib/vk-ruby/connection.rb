43fc7ae8cabaad2f15384ad5d38c6a2217223ec1
Andrew Zinenko <zinenkoan@gmail.com>
2012-03-16 00:28:55 +0400

    + escape to httpp params

--- lib/vk-ruby/connection.rb
+++ lib/vk-ruby/connection.rb
@@ -1,43 +1,54 @@
+require 'cgi'
+
 class VK::Connection
-  def initialize options = {}
-    options.each{|attr, value| instance_variable_set("@#{attr}", value) }
-    @http = create_connection
-  end
 
-  def request(verbs, path, data, &block)
-    @logger.debug "Request #{verbs} #{path} #{data}" if @logger
+	def initialize options = {}
+		options.each{|attr, value| instance_variable_set("@#{attr}", value) }
 
-    params = data.map{|k,v| "#{k}=#{v}"}.join('&')
+		reset!
+	end
 
-    requester = Proc.new{
-      case verbs
-        when :get
-          @http.get([path, params].join('?')).body
-        when :post
-          @http.post(path, params).body
-        else
-          raise 'Not suported http verbs'
-        end
-    }.call
+	def request(verbs, path, data)
+		@logger.debug "Request #{verbs} #{path} #{data}" if @logger
 
-  rescue Errno::EPIPE, Timeout::Error, Errno::EINVAL, EOFError
-    @logger.error "Rrequest errors(#{@attempts})"if @logger
-    @http = create_connection
-    (@attempts ||= 3) <= 0 ? raise : (@attempts -= 1; retry)
-  end
+		params = http_params(data)
 
-  private
+		requester = Proc.new{
+			case verbs
+				when :get
+					@http.get([path, params].join('?')).body
+				when :post
+					@http.post(path, params).body
+				else
+					raise 'Not suported http verbs'
+				end
+		}.call
 
-    def create_connection
-      net = Net::HTTP.new @host, @port
+	rescue Errno::EPIPE, Timeout::Error, Errno::EINVAL, EOFError
+		@logger.error "Rrequest errors(#{@attempts})"if @logger
+		reset!
+		(@attempts ||= 3) <= 0 ? raise : (@attempts -= 1; retry)
+	end
 
-      net.set_debug_output @logger if @logger && @logger.debug?
+	private
 
-      if @port == 443
-        net.use_ssl = true
-        net.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-      net 
-    end
-    
+	def reset!
+		@http = Net::HTTP.new @host, @port
+
+		if @logger && @logger.debug?
+			@http.set_debug_output @logger 
+		end
+		
+		if @port == 443
+			@http.use_ssl = true
+			@http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		end
+
+		@http 
+	end
+
+	def http_params(hash)
+		hash.map{|k,v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"}.join('&')
+	end
+
 end
