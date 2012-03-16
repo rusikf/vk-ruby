d974b49ac7c69619ea7b0169290c1a28cba862ed
Andrew Zinenko <zinenkoan@gmail.com>
2012-03-16 14:43:51 +0400

    + parsing bad profiles test

---
+++ tests/parsing_test.rb
@@ -0,0 +1,18 @@
+require File.expand_path('../helpers', __FILE__)
+
+class ParsingTest < MiniTest::Unit::TestCase
+	def setup
+		@app = VK::Standalone.new :app_id => 2657696
+		@fields = %w[uid first_name last_name nickname screen_name bdate sex city country photo_big has_mobile contacts education rate relation counters]
+	end
+
+	def test_bad_profiles
+		WebMock.allow_net_connect!
+
+		[22651572, 21774501].each do |uid|
+			@app.getProfiles :uids => uid, :fields => @fields.join(',')
+		end
+
+		WebMock.disable_net_connect!
+  end
+end
